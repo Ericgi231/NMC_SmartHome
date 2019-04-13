f7b75825c417027187beb964095bb9a72a98cb93
Eric Grant <ericgi231@gmail.com>
2019-04-13 15:45:08 -0400

    fixed user support

--- record.py
+++ record.py
@@ -8,15 +8,16 @@ pin = 4
 with open('config.json') as data_file:
 	data = json.load(data_file)
 
-DeviceId = int(str(data["UserId"]) + str(data["UnitId"]))
+userId = int(data["UserId"])
+unitId = int(data["UnitId"])
 
 humid, temp = Adafruit_DHT.read_retry(sensor, pin)
 
 cnx = mysql.connector.connect(user='SmartUser', password='Password1', host='108.174.56.152', port='3306', database='SmartHome')
 
 cur = cnx.cursor()
-sql = "INSERT INTO Climate (Id, DeviceId, Humidity, Temperature, RecordTime) VALUES (DEFAULT, %s, %s, %s, DEFAULT)"
-val = (DeviceId, humid, temp)
+sql = "INSERT INTO Climate (Id, UserId, UnitId, Humidity, Temperature, RecordTime) VALUES (DEFAULT, %s, %s, %s, %s, DEFAULT)"
+val = (userId, unitId, humid, temp)
 
 cur.execute(sql, val)
 cnx.commit()
